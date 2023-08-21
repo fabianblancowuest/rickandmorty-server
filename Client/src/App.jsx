c5673aa90b118e60f3eee454a286a2540a801d4e
Fabian Blanco Wuest <fabianblancowuest@gmail.com>
2023-08-21 18:41:19 -0300

    Limpiando el componente App

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -7,19 +7,16 @@ import Detail from "./components/Detail/Detail";
 import Form from "./components/Form/Form";
 import axios from "axios";
 import "./App.css";
-import Swal from "sweetalert2";
 import Favorites from "./components/Favorites/Favorites";
+import { swalFire1, swalFire2, swalFire3 } from "./sweetAlert";
+import Swal from "sweetalert2";
 
 function App() {
-	// ** Estado Inicial del arreglo de cards
-	const [characters, setCharacters] = useState([]);
-	// ** Estado incial del acceso al login
-	const [access, setAccess] = useState(false);
-	//** Estado inicial para validar que el usuario este registrado */
-	const [datos, setDatos] = useState({});
+	const [characters, setCharacters] = useState([]); // ** Estado Inicial del arreglo de cards
+	const [access, setAccess] = useState(false); // ** Estado incial del acceso al login
+	const [datos, setDatos] = useState({}); //** Estado inicial para validar que el usuario este registrado */
 
-	// ** Usuario y contraseña autorizado para el ingreso
-	const navigate = useNavigate();
+	const navigate = useNavigate(); // ** Usuario y contraseña autorizado para el ingreso
 
 	function login(userData) {
 		const { email, password } = userData;
@@ -30,10 +27,6 @@ function App() {
 			access && navigate("/home");
 		});
 	}
-
-	const img =
-		"https://s.yimg.com/ny/api/res/1.2/ERF8gU34MVP46JXFYeTvQQ--/YXBwaWQ9aGlnaGxhbmRlcjt3PTY0MDtoPTM2MA--/https://media.zenfs.com/en/cinemablend_388/4cea827a41d7c66770e144612647cf50";
-
 	//**Función para agregar personajes
 	function onSearch(id) {
 		// axios(`https://rickandmortyapi.com/api/character/${id}`)
@@ -45,32 +38,11 @@ function App() {
 			})
 			//! Captura el error para que no rompa la app!
 			// .catch((err) => alert(err.response.data));
-			.catch(() =>
-				// alert("No existen personajes con ese ID\nIngrese un ID válido"),
-				Swal.fire({
-					title: "¡ID no válido!",
-					// background: "#333",
-					color: "red",
-					text: "Debe ingresar un ID entre 1 y 826",
-					imageUrl: img,
-					imageWidth: 400,
-					imageHeight: 200,
-					imageAlt: "Wrong ID",
-				}),
-			);
+			.catch(() => swalFire1());
 	}
 	// **Función para limpiar las cards de la pantalla
 	function handleCleanScreen() {
-		Swal.fire({
-			title: "¿Estás seguro?",
-			text: "¡No podrás revertir esto!",
-			icon: "warning",
-			showCancelButton: true,
-			cancelButtonText: "Cancelar",
-			confirmButtonColor: "#3085d6",
-			cancelButtonColor: "#d33",
-			confirmButtonText: "¡Si, limpiala!",
-		}).then((result) => {
+		swalFire2().then((result) => {
 			if (result.isConfirmed) {
 				setCharacters([]);
 				Swal.fire("¡Listo!", "Se ha limpiado la pantalla.", "success");
@@ -90,22 +62,13 @@ function App() {
 
 	// ** Función para desloguearse
 	function handleLogout() {
-		Swal.fire({
-			title: "¿Estás seguro que quieres salir?",
-			showDenyButton: true,
-			confirmButtonColor: "green",
-			denyButtonText: "No, cancelar.",
-			// showCancelButton: true,
-			confirmButtonText: "Si, salir.",
-			// denyButtonText: `Don't save`,
-		}).then((result) => {
+		swalFire3().then((result) => {
 			/* Read more about isConfirmed, isDenied below */
 			if (result.isConfirmed) {
 				// Swal.fire("Saved!", "", "success");
 				setAccess(false);
 				setCharacters([]);
 				navigate("/");
-				// handleCleanScreen();
 			}
 			// else if (result.isDenied) {
 			// 	Swal.fire("Changes are not saved", "", "info");
